cb9151ef10719b9ee2c98e00e9618d4c19d49224
mr-moto <khash615@gmail.com>
2017-03-20 10:18:23 -0700

    fixed header problem

--- src/app/apiService/encounters.ts
+++ src/app/apiService/encounters.ts
@@ -1,11 +1,12 @@
 import{ Injectable} from '@angular/core';
-import { Http, Response} from '@angular/http';
+import { Http, Headers, Response} from '@angular/http';
 
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 import { Encounter, NewEncounter } from '../models';
 import { ENCOUNTERS_URL } from '../models/API'
+
 
 interface EncounterPostRequest {
     encounter: NewEncounter;
